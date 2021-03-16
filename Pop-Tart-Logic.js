e133e7fdfb7a903e74d3482cc1c996b99f1c37e0
rayacooper <47796461+rayacooper@users.noreply.github.com>
2021-03-16 12:07:39 -0600

    Added JS file, fixed some CSS

---
+++ Pop-Tart-Logic.js
@@ -0,0 +1,32 @@
+const productsViewList = [
+
+    {
+        img: '',
+        header: "Fan Faves",
+        flavor: 'Click here for the classics. Do it.'
+    },
+
+    {
+        img: '',
+        header: 'New',
+        flavor: "Delicious new flavors even we can't believe we came up with. Click here for that new new."
+    },
+
+    {
+        img: '',
+        header: 'Limited Time Only',
+        flavor: "Flavors this tasty can't be around forever. It wouldn't be fair to other food."
+    },
+
+    {
+        img: '',
+        header: "Bites & Crisps",
+        flavor: "Smaller Pop-Tarts flavors with big Pop-Tart energy."
+    },
+
+    {
+        img: '',
+        header: 'All Pop-Tarts Flavors',
+        flavor: "Can't decide? Click here and be extra torn."
+    }
+]
